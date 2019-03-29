b16bc6973b0dfb88e62b4f8171cd2f87267ca282
sunflow3r <slaven.bestvina@gmail.com>
2019-03-29 18:40:24 +0100

    modeli

---
+++ vjezba2.1_matematicki_izraz/Controllers/OsobaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vjezba2._1_matematicki_izraz.Models;
+
+namespace vjezba2._1_matematicki_izraz.Controllers
+{
+    public class OsobaController : Controller
+    {
+        // GET: Osoba
+        public ActionResult PopuniOsobu()
+        {
+            return View();
+        }
+        //POST :Osoba/PopuniOsobu
+        [HttpPost]
+        public ActionResult PopuniOsobu(Osoba osoba)
+        {
+            return View(osoba);
+        }
+    }
+}
---
+++ vjezba2.1_matematicki_izraz/Controllers/SimpleBindController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vjezba2._1_matematicki_izraz.Controllers
+{
+    public class SimpleBindController : Controller
+    {
+        // GET: SimpleBind/SimpleBindMetoda
+        public ViewResult SimpleBindMetoda()
+        {
+            return View("SimpleBind");
+        }
+        // samo preko akcije i ako akcija ima metodu post
+        [HttpPost]
+        public ViewResult SimpleBindMetoda(string ime)
+        {
+            string pozdrav = "Pozdrav," + ime + "!";
+            return View("SimpleBind", (object)pozdrav);
+        }
+    }
+}
