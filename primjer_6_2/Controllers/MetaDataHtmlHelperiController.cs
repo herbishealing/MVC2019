7979bfd42a77e155108c687b41e82007f2ea0af1
sunflow3r <slaven.bestvina@gmail.com>
2019-04-04 18:36:56 +0200

    metadata

---
+++ primjer_6_2/Controllers/MetaDataHtmlHelperiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using primjer_6_2.Models;
+namespace primjer_6_2.Controllers
+{
+    public class MetaDataHtmlHelperiController : Controller
+    {
+        // GET: MetaDataHtmlHelperi
+        public ViewResult MetaDataView()
+        {
+            return View(new OsobaMeta());
+        }
+        [HttpPost]
+        public ViewResult MetaDataView(OsobaMeta osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
